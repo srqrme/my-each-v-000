2669931f308f2b3a07108e09f5f579e54bb3d594
Sharmaine Requerme <srqrme@gmail.com>
2018-07-27 22:22:47 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,9 +1,9 @@
-def my_each(array) # put argument(s) here
+def my_each(collection) # put argument(s) here
   # code here
   i = 0
-  while i < array.length
-    yield(array[i])
+  while i < collection.length
+    yield(collection[i])
     i = i + 1
   end
-  array
+  collection
 end
